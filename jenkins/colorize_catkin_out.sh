6bffa70048b008e4c61246b0f136beb817b3d72a
Triantafyllos Afouras <afourast@gmail.com>
2014-03-14 03:41:19 +0200

    added colorization for gtests

--- jenkins/colorize_catkin_out.sh
+++ jenkins/colorize_catkin_out.sh
@@ -10,6 +10,7 @@ wht='\x1b[0;37m' # White
 err='\x1b[37;41m' # Error
 war='\x1b[30;43m' # Warning
 
+IFS='' # dunno why needed but if not, read ignores multiple spaces
 while read data; do
   echo $data | sed \
 -e "s%^\(\[.*\%\]\)\( Building CXX object.*$\)%\1$grn\2$blk%" \
@@ -19,8 +20,11 @@ while read data; do
 -e "s%^\(#### Running command: \)\(\".*\"\)\( in \)\(\".*\"$\)%$blu\1$cyn\2$blu\3$cyn\4$blk%" \
 -e "s%^####%$blu&$blk%" \
 -e "s%^.*: error: .*$%$err&$blk%" \
--e "s%^.*: warning: .*$%$war&$blk%"  ;
-done
+-e "s%^.*: warning: .*$%$war&$blk%"  \
+-e "s%^\(\[==========\]\|\[----------\]\|\[ *RUN *\]\|\[ *OK *\]\|\[ *PASSED *\]\)%$grn&$blk%" \
+-e "s%^\[ *FAILED *\]%$red&$blk%" ;
 
+
+done
 
 
