702562ddc1543b9e202c05d5c047b88182d1c6cf
Chris Zalidis <zalidis@gmail.com>
2014-05-12 17:13:21 +0300

    Fix smtp port issue on server

--- mail_notifications/smtp_client.py
+++ mail_notifications/smtp_client.py
@@ -1,10 +1,10 @@
 import smtplib
 
-def send_mail(server, user, password, mail_message, sender, recipients):
+def send_mail(server, user, password, port, mail_message, sender, recipients):
   
   mail_message.replace_header('To', ', '.join(recipients))
 
-  session = smtplib.SMTP(server)
+  session = smtplib.SMTP(server, int(port))
   session.login(user, password)
   session.sendmail(sender, recipients, mail_message.as_string())
   session.quit()
--- mail_notifications/travis_notifications.py
+++ mail_notifications/travis_notifications.py
@@ -19,15 +19,19 @@ def get_credentials():
   credentials = yaml.safe_load(doc)
   doc.close()
 
-  return credentials['server_name'], credentials['user_name'], credentials['password'], credentials['imap_port']
+  return (credentials['server_name'],
+          credentials['user_name'],
+          credentials['password'],
+          credentials['imap_port'],
+          credentials['smtp_port'])
 
 def get_commit_from_mail(mail):
 
   return re.findall(r'Commit: ([0-9a-z]+)', mail.as_string())[0]
 
-def check_and_forward_mail(server, user, password, port):
+def check_and_forward_mail(server, user, password, imap_port, smtp_port):
 
-  new_emails = imap_client.connect_and_check_imap(server, user, password, port)
+  new_emails = imap_client.connect_and_check_imap(server, user, password, imap_port)
 
   for mail in new_emails:
     commit_sha = get_commit_from_mail(mail)
@@ -38,8 +42,8 @@ def check_and_forward_mail(server, user, password, port):
     print 'New mail, Subject:', mail['Subject'] 
 
     recipients += default_recipients
-    smtp_client.send_mail(server, user, password, mail, mail['From'], set(recipients))
+    smtp_client.send_mail(server, user, password, smtp_port, mail, mail['From'], set(recipients))
 
 if __name__ == '__main__':
-  server, user, password, port = get_credentials()
-  check_and_forward_mail(server, user, password, port)
+  server, user, password, imap_port, smtp_port = get_credentials()
+  check_and_forward_mail(server, user, password, imap_port, smtp_port)
